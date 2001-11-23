b5e46b7a0ffd865e7b04ced46a99a5908a37300f
Rob Kaper <cap@capsi.com>
2001-11-23 08:34:54 +0000

    actual skeleton
    svn path=/trunk/kdegames/atlantik/; revision=123782

---
+++ src/trade_widget.cpp
@@ -0,0 +1,5 @@
+#include "trade_widget.moc"
+
+TradeDisplay::TradeDisplay(QWidget *parent, const char *name) : QWidget(parent, name)
+{
+}
---
+++ src/trade_widget.h
@@ -0,0 +1,14 @@
+#include <qwidget.h>
+//#include <qlayout.h>
+//#include <qvgroupbox.h>
+//#include <qlistview.h>
+//#include <qpushbutton.h>
+//#include <qtextedit.h>
+
+class TradeDisplay : public QWidget
+{
+Q_OBJECT
+
+public:
+	TradeDisplay(QWidget *parent, const char *name = 0);
+};
