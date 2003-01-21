5a775003080397f6b4c15df5e14c91d5dbd5c957
Rob Kaper <cap@capsi.com>
2003-01-21 02:44:31 +0000

    update copyrights
    svn path=/trunk/kdegames/atlantik/; revision=201604

--- client/selectconfiguration_widget.h
+++ client/selectconfiguration_widget.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2002 Rob Kaper <cap@capsi.com>
+// Copyright (c) 2002-2003 Rob Kaper <cap@capsi.com>
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
