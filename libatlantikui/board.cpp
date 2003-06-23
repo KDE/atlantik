7aa1c28c2d2b711e99e06db5895c1a463976d659
Rob Kaper <cap@capsi.com>
2003-06-23 03:05:27 +0000

    update copyright headers
    svn path=/trunk/kdegames/atlantik/; revision=233137

--- libatlantikui/board.cpp
+++ libatlantikui/board.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2002 Rob Kaper <cap@capsi.com>
+// Copyright (c) 2002-2003 Rob Kaper <cap@capsi.com>
 //
 // This library is free software; you can redistribute it and/or
 // modify it under the terms of the GNU Lesser General Public
