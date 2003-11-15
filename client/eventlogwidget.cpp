03b6be71515a2ed061af5ad1aa14df17a921fb89
Rob Kaper <cap@capsi.com>
2003-11-15 13:31:04 +0000

    eventlog and statusbar support
    svn path=/trunk/kdegames/atlantik/; revision=267242

---
+++ client/eventlogwidget.cpp
@@ -0,0 +1,92 @@
+// Copyright (c) 2003 Rob Kaper <cap@capsi.com>
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License version 2.1 as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this library; see the file COPYING.LIB.  If not, write to
+// the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+// Boston, MA 02111-1307, USA.
+
+#include <iostream>
+
+#include <qlayout.h>
+#include <qdatetime.h>
+
+#include <klocale.h>
+#include <klistview.h>
+#include <kdialogbase.h>
+#include <kiconloader.h>
+#include <kpushbutton.h>
+
+#include "event.h"
+#include "eventlogwidget.moc"
+
+EventLog::EventLog()
+{
+}
+
+void EventLog::addEvent(const QString &description, const QString &icon)
+{
+	Event *event = new Event(QDateTime::currentDateTime(), description, icon);
+	m_events.append(event);
+	emit newEvent(event);
+}
+
+void EventLog::emitAll()
+{
+	for (QPtrListIterator<Event> it(m_events); (*it) ; ++it)
+		emit newEvent((*it));
+}
+
+EventLogWidget::EventLogWidget(EventLog *eventLog, QWidget *parent, const char *name)
+	: QWidget(parent, name,
+	  WType_Dialog | WStyle_Customize | WStyle_DialogBorder | WStyle_Title |
+	  WStyle_Minimize | WStyle_ContextHelp )
+{
+	m_eventLog = eventLog;
+
+	connect(m_eventLog, SIGNAL(newEvent(Event *)), this, SLOT(addEvent(Event *)));
+
+	setCaption(i18n("Event Log"));
+
+	QVBoxLayout *listCompBox = new QVBoxLayout(this, KDialog::marginHint());
+
+	m_eventList = new KListView(this, "eventList");
+	listCompBox->addWidget(m_eventList);
+
+	m_eventList->addColumn(i18n("Date/Time"));
+	m_eventList->addColumn(i18n("Description"));
+
+	QHBoxLayout *actionBox = new QHBoxLayout(this, 0, KDialog::spacingHint());
+	listCompBox->addItem(actionBox);
+
+	actionBox->addItem(new QSpacerItem(20, 20, QSizePolicy::Expanding, QSizePolicy::Minimum));
+
+	m_saveButton = new KPushButton(BarIcon("save", KIcon::SizeSmall), i18n("Save"), this);
+	actionBox->addWidget(m_saveButton);
+
+	connect(m_saveButton, SIGNAL(clicked()), this, SLOT(save()));
+}
+
+void EventLogWidget::addEvent(Event *event)
+{
+	KListViewItem *item = new KListViewItem(m_eventList, event->dateTime().toString("yyyy-MM-dd hh:mm:ss zzz"), event->description());
+	if (event->icon().isEmpty())
+		item->setPixmap(1, QPixmap(SmallIcon("atlantik")));
+	else
+		item->setPixmap(1, QPixmap(SmallIcon(event->icon())));
+
+	m_eventList->ensureItemVisible(item);
+}
+
+void EventLogWidget::closeEvent(QCloseEvent *e)
+{
+	e->accept();
+}
