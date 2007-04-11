19e4271b971d39fdf7e6fded65583b5f4fd58def
Laurent Montel <montel@kde.org>
2007-04-11 14:33:28 +0000

    Fix include
    svn path=/trunk/KDE/kdegames/atlantik/; revision=652589

--- libatlantikui/auction_widget.cpp
+++ libatlantikui/auction_widget.cpp
@@ -14,7 +14,7 @@
 // the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 // Boston, MA 02110-1301, USA.
 
-#include <q3hbox.h>
+#include <khbox.h>
 #include <QSpinBox>
 #include <QLabel>
 //Added by qt3to4:
@@ -76,7 +76,7 @@ AuctionWidget::AuctionWidget(AtlanticCore *atlanticCore, Auction *auction, QWidg
 	}
 
 	// Bid spinbox and button
-	Q3HBox *bidBox = new Q3HBox(this);
+	KHBox *bidBox = new KHBox(this);
 	m_mainLayout->addWidget(bidBox);
 
 	m_bidSpinBox = new QSpinBox(bidBox);
